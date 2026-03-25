5b18f6c163c7220ad1d5d9cdb22588a23e1310fc
Alan <maximob1@htomai.com>
2026-03-25 08:24:58 -0600

    segunda parte

---
+++ herramienta.java
@@ -0,0 +1,9 @@
+public class herramienta {
+
+    public int iva (){
+        
+    }
+
+
+
+}
--- solucionU3.java
+++ solucionU3.java
@@ -1,5 +1,5 @@
  import java.util.Scanner;
- 
+
  public class solucionU3{
 
     public static void main(String[] args) {
@@ -9,7 +9,9 @@
     int opc=0;
 
      while (opc != 3) {
-
+    
+    System.out.println();
+    System.out.println("-----Sistema de Auditoria Tech-----");
     System.out.println("1. Auditar Facturas");
     System.out.println("2. Calculadora de Impuestos");
     System.out.println("3. Cerrar Turno");
@@ -17,7 +19,28 @@
     System.out.print("Elige una opcion: ");
     opc =scan.nextInt();
     
-    int y = 0;
+     int ttCobrado=0, suma=0;   
+    if (opc == 1) {
+       
+    System.out.print("¿Cuantas Facturas se emitieron durante el dia? :");
+    int facalDia =scan.nextInt() ;
+
+    /*Primera opcion */
+    for (int i = 0; i < facalDia; i++){
+    System.out.print("Monte de la Factura: ");
+    ttCobrado =scan.nextInt();
+    suma = suma + ttCobrado;
+    }   
+    System.out.println("Corte de caja Total: "+ suma);
+    }
+    
+    if (opc == 2) {
+
+    System.out.print("Escribe el precio bruto de un coponente: ");
+    int pBrutoC = scan.nextInt();
+    } else {
+        
+    }
 
    
        
