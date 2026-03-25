3c0fbd973a37a98b5601d5e835c9845536041623
Alan <maximob1@htomai.com>
2026-03-25 08:08:31 -0600

    primera parte

--- solucionU3.java
+++ solucionU3.java
@@ -1,1 +1,51 @@
+ import java.util.Scanner;
+ 
+ public class solucionU3{
+
+    public static void main(String[] args) {
+        
+    Scanner scan = new Scanner(System.in);
+
+    int opc=0;
+
+     while (opc != 3) {
+
+    System.out.println("1. Auditar Facturas");
+    System.out.println("2. Calculadora de Impuestos");
+    System.out.println("3. Cerrar Turno");
+
+    System.out.print("Elige una opcion: ");
+    opc =scan.nextInt();
+    
+    int y = 0;
+
+   
+       
+       
+        
+    }  
+    }
+   
+
+
+
+
+
+     /* while (x == 1) {
+
+   System.out.print("¿Cuantas Facturas se emitieron durante el dia? :");
+    int facalDia =scan.nextInt() ;
+
+    for (int i = 0; i <= facalDia; i++){
+
+    System.out.print("Total cobrado: ");
+    int ttCobrado =scan.nextInt();
+    int suma = ttCobrado + suma;
+    System.out.println("Corte de caja Total: "+ suma);
+    }
+    
+        
+    }*/
+    }
+
 
